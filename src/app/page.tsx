245f5453615a79aed186c15b5cddab7a17df66fe
Bijay <bijay.subedi837@gmail.com>
2025-03-23 20:20:18 +0545

    feat: comming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ const Page = () => {
   return (
     <div className="flex flex-col gap-2 min-h-[400px] items-center justify-center w-full">
       <span>Sports Mandu Nepal</span>
+      <span>All sports solutions in one place</span>
       <span className="animate-pulse text-[14px]">Comming Soon...</span>
     </div>
   );
