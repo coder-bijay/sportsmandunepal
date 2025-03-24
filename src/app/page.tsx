27b1fd851d0e6e781d2d645e7a78f28074691ac1
Bijay <bijay.subedi837@gmail.com>
2025-03-24 12:31:21 +0545

    feat: added the colors in comming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
+import ComingSoon from "../common/components/CommingSoon";
 
 const Page = () => {
   return (
-    <div className="flex flex-col gap-2 min-h-[400px] items-center justify-center w-full">
-      <span>Sports Mandu Nepal</span>
-      <span>All sports solutions in one place</span>
-      <span className="animate-pulse text-[14px]">Comming Soon...</span>
-    </div>
+    <>
+      <ComingSoon />
+    </>
   );
 };
 
