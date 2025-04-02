5f24f8ce15f8082d4ba0befa5f2507bc753caa84
Bijay <bijay.subedi837@gmail.com>
2025-04-02 20:28:10 +0545

    feat: change the text size according to screen

--- src/common/components/CommingSoon.tsx
+++ src/common/components/CommingSoon.tsx
@@ -4,17 +4,17 @@ import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 export default function ComingSoon() {
   return (
     <div className="flex items-center justify-center min-h-screen w-full bg-gradient-to-r from-purple-500 to-pink-500 px-4">
-      <div className="w-1/2 rounded-lg p-8 text-center">
-        <h1 className="text-4xl font-extrabold text-white drop-shadow-lg">
+      <div className="lg:w-1/2 w-full rounded-lg p-8 text-center">
+        <h1 className="text-xl md:text-3xl lg:text-4xl font-extrabold text-white drop-shadow-lg">
           Sports Mandu Nepal
         </h1>
-        <p className="mt-3 text-lg text-white/80">
+        <p className="mt-3 text-sm md:text-base lg:text-lg text-white/80">
           All sports solutions in one place
         </p>
-        <p className="mt-3 text-base text-white/80">
+        <p className="mt-3 text-sm lg:text-base text-white/80">
           Top-quality sports gear at wholesale & retail rates.
         </p>
-        <p className="mt-5 text-lg text-yellow-400 animate-pulse font-semibold uppercase">
+        <p className="mt-5 text-sm md:text-base lg:text-lg text-yellow-400 animate-pulse font-semibold uppercase">
           Coming Soon...
         </p>
 
