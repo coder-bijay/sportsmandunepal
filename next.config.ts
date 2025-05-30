1ae7a03b2f6012a6c038ee482bf6b41bab76373a
Bijay Subedi <bijay.subedi837@gmail.com>
2025-05-30 12:48:42 +0545

    feat: integrate the api to fetch all the products

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "/dcgdhd1er/image/upload/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
