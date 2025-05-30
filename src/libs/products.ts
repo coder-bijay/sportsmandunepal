9f97c7aade9a9e3e3d0158b4469e78df391c8f05
Bijay Subedi <bijay.subedi837@gmail.com>
2025-05-30 22:40:39 +0545

    fix: added the base url on server side rendered apis

--- src/libs/products.ts
+++ src/libs/products.ts
@@ -1,9 +1,12 @@
 import { cache } from "react";
 
 export const getProducts = cache(async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/product`, {
-    next: { revalidate: 3600 },
-  });
+  const res = await fetch(
+    `https://ecommerce-backend-k792.onrender.com/api/v1/product`,
+    {
+      next: { revalidate: 3600 },
+    }
+  );
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 });
