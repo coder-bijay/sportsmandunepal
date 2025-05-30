5147d35a3e4f200cb32df9a171a0f81dbef9ee44
Bijay Subedi <bijay.subedi837@gmail.com>
2025-05-30 22:24:46 +0545

    change the base url on env

--- src/libs/products.ts
+++ src/libs/products.ts
@@ -1,8 +1,7 @@
 import { cache } from "react";
 
 export const getProducts = cache(async () => {
-  //   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/product`, {
-  const res = await fetch(`http://localhost:5000/api/v1/product`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/product`, {
     next: { revalidate: 3600 },
   });
   if (!res.ok) throw new Error("Failed to fetch products");
