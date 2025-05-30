da6f1f4355c4cb51ef684d8ae1b5674d57f4173b
Bijay Subedi <bijay.subedi837@gmail.com>
2025-05-30 13:03:55 +0545

    fix: hot fixes

--- src/libs/products.ts
+++ src/libs/products.ts
@@ -1,7 +1,8 @@
 import { cache } from "react";
 
 export const getProducts = cache(async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/product`, {
+  //   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/product`, {
+  const res = await fetch(`http://localhost:5000/api/v1/product`, {
     next: { revalidate: 3600 },
   });
   if (!res.ok) throw new Error("Failed to fetch products");
