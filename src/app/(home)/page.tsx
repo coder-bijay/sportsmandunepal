2e799da400b719965990487a7cbc0fda99bee8a6
Bijay Subedi <bijay.subedi837@gmail.com>
2025-06-24 13:31:22 +0545

    feat: testing development deployment

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
       {/* <ComingSoon /> */}
 
       <div className="container min-h-[400px] flex items-center justify-center flex-col mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+        <h1 className="text-3xl font-bold mb-4">Welcome, Home Page</h1>
         <p className="text-lg text-gray-700">
           This is the home page of our application. Explore our features and
           products!
