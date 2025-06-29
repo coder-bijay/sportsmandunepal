8d7897ae2f61d1fec175d5af786b945aa22f01b1
Bijay Subedi <bijay.subedi837@gmail.com>
2025-06-29 22:29:44 +0545

    fix: resolving the build issue

--- src/app/(routes)/products/featured-products/page.tsx
+++ src/app/(routes)/products/featured-products/page.tsx
@@ -2,6 +2,7 @@ import { getProducts } from "@/src/libs/products";
 import React from "react";
 import { ProductCard } from "../components/ProductCard";
 import { IProduct } from "../interface";
+import { notFound } from "next/navigation";
 
 const ProductGrid = async () => {
   const products = await getProducts({
@@ -9,16 +10,22 @@ const ProductGrid = async () => {
     value: "featured",
   });
 
+  if (!products || !products.data) {
+    notFound();
+  }
+
+  const productList = products?.data;
+
   return (
     <div className="container mx-auto p-2 flex flex-col gap-4 lg:p-4">
       <h1 className="text-2xl font-bold">Featured Products</h1>
-      {products?.data?.length === 0 ? (
+      {productList?.length === 0 ? (
         <div className="text-center text-gray-500">
           No featured products found.
         </div>
       ) : (
         <div className="flex flex-wrap">
-          {products?.data?.map((product: IProduct, index: number) => (
+          {productList?.map((product: IProduct, index: number) => (
             <ProductCard
               key={index}
               slug={product.slug}
--- src/app/(routes)/products/new-arrivals/page.tsx
+++ src/app/(routes)/products/new-arrivals/page.tsx
@@ -2,6 +2,7 @@ import { getProducts } from "@/src/libs/products";
 import React from "react";
 import { ProductCard } from "../components/ProductCard";
 import { IProduct } from "../interface";
+import { notFound } from "next/navigation";
 
 const ProductGrid = async () => {
   const products = await getProducts({
@@ -9,15 +10,21 @@ const ProductGrid = async () => {
     value: "new_arrivals",
   });
 
+  if (!products || !products.data) {
+    notFound();
+  }
+
+  const productList = products?.data;
+
   return (
     <div className="container mx-auto p-2 flex flex-col gap-4 lg:p-4">
       <h1 className="text-2xl font-bold">New Arrivals</h1>
 
-      {products?.data?.length === 0 ? (
+      {productList?.length === 0 ? (
         <div className="text-center text-gray-500">No new arrivals found.</div>
       ) : (
         <div className="flex flex-wrap">
-          {products?.data?.map((product: IProduct, index: number) => (
+          {productList?.map((product: IProduct, index: number) => (
             <ProductCard
               key={index}
               slug={product?.slug}
